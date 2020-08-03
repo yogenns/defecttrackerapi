a47d06b59c49c3f155bd4d8d54e099193c0854b0
yogenns <yogenns@gmail.com>
2020-08-03 16:32:21 +0530

    Task 1 - Create API Project in GitHub

---
+++ src/test/java/com/yogendra/webservices/defecttrackerapi/DefectTrackerApiApplicationTests.java
@@ -0,0 +1,9 @@
+package com.yogendra.webservices.defecttrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectTrackerApiApplicationTests {
+
+}
